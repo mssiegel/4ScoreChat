8471bd20bac34d789561120e5d65f1df20ae1f27
Moshe Siegel <siegel.moshes@gmail.com>
2019-10-07 13:15:50 -0400

    1. Internet check 2. Sticky buttons
    Checks for internet when starting a new chat.  Also made chat buttons stick to top of viewport

--- src/components/ChatroomPage.js
+++ src/components/ChatroomPage.js
@@ -51,9 +51,12 @@ const ChatroomPage = () => {
 
   //EMIT socket events
 
-  function startChatBtnClicked(){
-    setWaitingForPeer(true)
-    socket.emit('enter chatQueue', chat.you)
+  async function startChatBtnClicked(){
+    const isInternet = await checkInternet()
+    if (isInternet) {
+      setWaitingForPeer(true)
+      socket.emit('enter chatQueue', chat.you)
+    }
   }
 
   function cancelSearch(){
@@ -63,6 +66,23 @@ const ChatroomPage = () => {
     socket.emit('end chat')
   }
 
+
+  async function checkInternet() {
+    //if no internet then request will thrown an Error
+    //no-cors mode requests opaque response thereby returning faster and providing extra safety against no-cors issue
+    const request = {method: 'HEAD', mode: 'no-cors'};
+    try {
+      await fetch('https://www.4scorechat.com', request)
+      return true
+    }
+    catch(e) {
+      setIsInternet(false)
+      setTimeout(() => setIsInternet(true), 5000)
+      return false
+    }
+  }
+
+  const [isInternet, setIsInternet] = useState(true)
   const [waitingForPeer, setWaitingForPeer] = useState(false)
   const [chatInSession, setChatInSession] = useState(false)
 
@@ -87,6 +107,9 @@ const ChatroomPage = () => {
       {(!waitingForPeer && !chatInSession) &&  
         <button className={`start-chat btn ${chat.you ? '' : 'disabled'}`} disabled={!chat.you} 
         onClick={startChatBtnClicked}>Start Chat</button>
+      }
+      {!isInternet &&
+        <p className='no-internet'>Could not connect. Please check your internet connection and try again.</p>
       }
       {waitingForPeer && 
         <div className="waiting-for-peer"> 
