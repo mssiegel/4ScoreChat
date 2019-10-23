12eb2997b2df0e8469fced7d17bec3901e36b883
Moshe Siegel <siegel.moshes@gmail.com>
2019-10-22 22:55:12 -0400

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Moshe Siegel`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
