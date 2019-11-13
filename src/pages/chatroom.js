be1fd50fcfd31337253a98af0d22b856ff6bd479
Moshe Siegel <siegel.moshes@gmail.com>
2019-11-13 15:22:51 -0500

    Moved socket initialization outside of Chatroom component
    socket will now remain unchanged even if user clicks Chatroom link in navbar

--- src/pages/chatroom.js
+++ src/pages/chatroom.js
@@ -6,17 +6,17 @@ import SEO from "../components/seo"
 import ChatroomPage from "../components/ChatroomPage"
 import io from 'socket.io-client'
 
+//Set up socket.io connection to server
+//Initialize socket outside of Chatroom component so as to keep socket unchanged even if user clicks Chatroom link in navbar
+//If client is at 4scorechat domain name then use heroku backend server, else use local host backend server
+//const serverUrl = window.location.href === 'https://www.4scorechat.com/' ? 'https://scorechat.herokuapp.com/' : 'http://localhost:8000'
+const serverUrl = 'https://scorechat.herokuapp.com/'  
+const socket = io(serverUrl)
+
 const Chatroom = () => {
-  //Set up socket.io connection to server
-  //If client is at 4scorechat domain name, use heroku backend server else use local host backend server
-  //const serverUrl = window.location.href === 'https://www.4scorechat.com/' ? 'https://scorechat.herokuapp.com/' : 'http://localhost:8000'
-  const serverUrl = 'https://scorechat.herokuapp.com/'
-  const socket = io(serverUrl)
-  useEffect(()=> {
-   return () => {
-     socket.disconnect()
-   } 
-  }, []) 
+  socket.connect()
+  //disconnects socket when user leaves Chatroom Page
+  useEffect(() => () => socket.disconnect(), [])
 
   const tips = ['feel free to EXAGGERATE your words by using capital letters', 'use your partner for leverage. Together, shoot for the skies', 'for comedy, agree with your peer and dramatize what they say', 'smile because it will make you happier', 'do not keep your peers waiting while you write long pagragraphs. Instead, write shorter lines', 'blank your mind to the past and future. Stay focused on the NOW.', 'choose whatever crazy character tickles your fancy', 'do not enter with a predetermined storyline. Enter with a blank mind. Create the plot with each back-and-forth response', 'try to get better and better. When your chat ends, review it. Look for ways to improve your chatting skills', 'cooperation works wonders. See your peer as your partner in creating a beautiful storyline', "chats can get intense. Come prepared with a box of tissues and a shoulder to lean on...or don't", 'partner with your peer to create deep, intense, emotional storylines']
   const randomTip  = tips[Math.floor(Math.random() * tips.length)]
