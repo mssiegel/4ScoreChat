f3d8e40c58d103f34d07ef0a9c870b38f1589404
Moshe Siegel <siegel.moshes@gmail.com>
2019-12-10 16:53:53 -0500

    Replaced randomTip with bestChattingTime on Chatroom Page

--- src/components/ChatroomPage.js
+++ src/components/ChatroomPage.js
@@ -60,7 +60,7 @@ const ChatroomPage = ({socket, randomTip}) => {
     //suggests a new character - always different than previous one
     let randomChar
     do {
-      const characterList = ['Whoopee Cushion Maker', 'Elven Archer', 'Powerful Circus Clown', 'Ballerina Spy', 'Website Builder', 'Funny Prankster', 'Mighty Knight', 'Dragon Slayer', 'Viking Warrior','Leader of Warrior Army','Laughing Jack', 'Plumbing SuperHero', '9th Level Wizard', 'Charming Beggar', 'Vampire Hunter', 'Alien Warlord', 'Green Tailed Alien', 'Pirate Captain', 'Laughing Sorcerer', 'Maniac Magician', 'Hypnotist Master', 'Defender of the Righteous', 'Job Applicant', 'Rebel Leader', 'Tiny Warlord', 'Dancing Champion', 'Scared Scuba Diver', 'Founder of Farters R Us', 'Psycho Therapist', 'News Reporter', 'Wealthy Merchant', 'Productivity Coach', 'Karate Dude', 'Forgetful Surgeon', 'Mighty Lawyer', 'Dude with Diaper Gun', 'Pro Water Gunner', 'Happy Gnome', 'Large Demon', 'Bloody Doctor', 'Party Animal Pirate', 'Galactic Bounty Hunter', 'Fuzzy Wuzzy Wrestler', 'Water Balloon Gangster', 'Boxing Champ', 'Adventurer', 'Minotaur in a Maze', 'Dungeon Troll', 'Sam the Sheriff', 'Alexyn, Lord of Dragons', 'Partying Swordsman', 'Zilod the Fighter', 'Pharix, Lord of Chaos', 'Green Ogre', 'Owner of Shop', 'The Witch of Zimbodia', 'Soccer Coach', 'The SuperHero', 'Zombie Goblin', 'Terrified of Heights', 'Unicycle Speed Racer', 'Afraid of Bugs', 'Lover of Spiders', 'Fortune Teller']
+      const characterList = ['Person at Wedding','Whoopee Cushion Maker', 'Elven Archer', 'Powerful Circus Clown', 'Ballerina Spy', 'Website Builder', 'Funny Prankster', 'Mighty Knight', 'Dragon Slayer', 'Viking Warrior','Leader of Warrior Army','Laughing Jack', 'Plumbing SuperHero', '9th Level Wizard', 'Charming Beggar', 'Vampire Hunter', 'Alien Warlord', 'Green Tailed Alien', 'Pirate Captain', 'Laughing Sorcerer', 'Maniac Magician', 'Hypnotist Master', 'Defender of the Righteous', 'Job Applicant', 'Rebel Leader', 'Tiny Warlord', 'Dancing Champion', 'Scared Scuba Diver', 'Founder of Farters R Us', 'Psycho Therapist', 'News Reporter', 'Wealthy Merchant', 'Productivity Coach', 'Karate Dude', 'Forgetful Surgeon', 'Mighty Lawyer', 'Dude with Diaper Gun', 'Pro Water Gunner', 'Happy Gnome', 'Large Demon', 'Bloody Doctor', 'Party Animal Pirate', 'Galactic Bounty Hunter', 'Fuzzy Wuzzy Wrestler', 'Water Balloon Gangster', 'Boxing Champ', 'Adventurer', 'Minotaur in a Maze', 'Dungeon Troll', 'Sam the Sheriff', 'Alexyn, Lord of Dragons', 'Partying Swordsman', 'Zilod the Fighter', 'Pharix, Lord of Chaos', 'Green Ogre', 'Owner of Shop', 'The Witch of Zimbodia', 'Soccer Coach', 'The SuperHero', 'Zombie Goblin', 'Terrified of Heights', 'Unicycle Speed Racer', 'Afraid of Bugs', 'Lover of Spiders', 'Fortune Teller']
       const randomNum = Math.floor(Math.random() * characterList.length)
       randomChar = characterList[randomNum]
     } while (randomChar === chat.you)
@@ -84,7 +84,9 @@ const ChatroomPage = ({socket, randomTip}) => {
     <>
     <section className="chatroom-page-wrapper">
       <h1 className="page-title">Chatroom</h1>
-      <p id="tip">Tip: {randomTip}</p>
+      <p id="tip">Tip: Add to your calendar!! <span className="tip-best-time">Sunday 8:00pm EST</span>. Guaranteed to be people online at that time, every Sunday.</p>
+      {/*{randomTip}*/}
+
       {!chatInSession &&
       <>
         <button className={`suggest-character btn ${waitingForPeer ? 'disabled' : ''}`} onClick={waitingForPeer ? null: suggestCharacter}>Suggest <MoodIcon style={characterIcon}/></button>
@@ -104,9 +106,9 @@ const ChatroomPage = ({socket, randomTip}) => {
       }
       {waitingForPeer && 
         <div className="waiting-for-peer"> 
-          <p className="waiting-note">Best chatting time is <span className="best-time">Sundays at 8:00pm EST</span>. Come then to get matched instantly!</p>
+          <p className="waiting-title">Looking for someone...</p>          
           <button className="btn" id='cancel-chat-btn' onClick={cancelSearch}>Cancel</button>
-          <p className="waiting-title">Looking for someone to pair you with...</p>          
+          <p className="waiting-note">Best chatting time is <span className="best-time">Sundays at 8:00pm EST</span>. Come then to get matched instantly!</p>
         </div>
       }
     </section>
