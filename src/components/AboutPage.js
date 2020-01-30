9504b026c2572852083110083edd41652c5b819d
Moshe Siegel <siegel.moshes@gmail.com>
2020-01-29 23:11:53 -0500

    updated About page text to tell the story of how 4ScoreChat came to be

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -13,10 +13,14 @@ const AboutPage = () => {
     <section className='page-wrapper'>
       <h1 className='page-title'>About</h1>
       <div className='common-center'>
-        <p>Pick your nose, fart out loud, eat your boogers, whatever, its all okay. Here, you can become whomever you want. Just choose a character and start chatting. Though we don't enforce it, we request all chats stay somewhat clean and family-friendly.</p>
-        <p>Each conversation is a chance to deeply connect with your fellow human. It's about getting in the flow, feeding off one another, and telling a story. Chat long enough and you may notice your imagination and eagerness improving.</p>
-        <p>You may experience some side effects though. People who use this site report feeling more empowered outside in the real-world. They say their friends tell them they're more exciting, more empathetic, and have neat imaginations. So now it is your turn. Go ahead and enter our chatroom.</p>
-        <p><b>Contact: </b>Reach out to us by email at <a href="mailto:4ScoreChat@gmail.com">4ScoreChat@gmail.com</a></p>
+        <p>4ScoreChat was started by a man from New York who is a fan of getting out of his comfort zone. He took improv classes at New York's Magnet Theater and also did public speaking at the New York Toastmasters Club. However, when life got busy, he wanted a way to improv and storytell from the comfort of his home. And so 4ScoreChat was born.</p>
+
+        <p>Our mission is to build a community that fosters imagination, self-improvement, and spontaneous improv. Though we don't enforce it, we request all chats on our site stay somewhat clean and family-friendly.</p>
+        
+        <p>If you have suggestions or ideas for improvement, then let us know. Also, we are based out of New York City and are looking for help with marketing and engineering (our tech stack is React/NodeJs). If you'd like to join our team, then send us an email explaining what value you can provide.</p>
+
+        <p><b>Contact: </b>Our email is <a href="mailto:4ScoreChat@gmail.com">4ScoreChat@gmail.com</a></p>
+        
         <p style={aboutDisclaimer}>Disclaimer: By using our site you agree to have fun, entertaining improv chats. You also acknowledge you can end a chat at any time and agree to not hold us responsible for any claims made during chats.</p>
       </div>
     </section>
