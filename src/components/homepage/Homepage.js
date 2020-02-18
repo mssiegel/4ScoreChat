f8c686536f0307b674affc56516c3be20cee0e06
Moshe Siegel <siegel.moshes@gmail.com>
2020-02-18 10:06:20 -0500

    Added Hero Boy image above enter chatroom button on homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,6 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
-import "./Homepage.css"
+import React from 'react'
+import { Link } from 'gatsby'
+import './Homepage.css'
+import heroBoy from '../../images/hero-boy.png'
 import Chatbox from '../chatbox/Chatbox'
 import ListItems from './ListItems'
 import EmailSignup from './EmailSignup'
@@ -9,12 +10,14 @@ const firstChat = {
   title: 'Be Anyone',
   you: 'Toy store owner',
   peer: 'Vampire hunter',
-  conversation : [
+  conversation: [
     ['peer', 'Vampire hunter', 'nice dagger you got'],
-    ['you', 'Toy store owner', "thanks. It's made of strong plastic."],
-    ['peer', 'Vampire hunter', "hmmm, I challenge you to a duel"],
-    ['you', 'Toy store owner', "haha bring it on bub"],
-    ['peer', 'Vampire hunter', "with pleasure"]
+    ['you', 'Toy store owner', 'thanks.'],
+    ['you', 'Toy store owner', "it's made of strong plastic."],
+    ['peer', 'Vampire hunter', 'hmmm'],
+    ['peer', 'Vampire hunter', 'I challenge you to a duel'],
+    ['you', 'Toy store owner', 'bring it on bub'],
+    ['peer', 'Vampire hunter', 'with pleasure']
   ]
 }
 
@@ -23,53 +26,57 @@ const secondChat = {
   titleColor: 'black',
   you: 'Pizza delivery boy',
   peer: 'Guitar rockstar',
-  conversation : [
+  conversation: [
     ['you', 'Pizza delivery boy', 'hello Guitar rockstar'],
     ['you', 'Pizza delivery boy', 'Can you sing me a song?'],
     ['peer', 'Guitar rockstar', "I'll sing for a smoking hot pizza"],
     ['you', 'Pizza delivery boy', "Okay, here's your pizza."],
-    ['peer', 'Guitar rockstar', "Gee thanks!"],
-    ['peer', 'Guitar rockstar', "I'll sing the pizza song!"],
-    ['you', 'Pizza delivery boy', "THANKS! I LOVE THE PIZZA SONG!"]
+    ['peer', 'Guitar rockstar', 'Gee thanks!'],
+    ['peer', 'Guitar rockstar', 'Oh pizza you taste so gooooood'],
+    ['you', 'Pizza delivery boy', "Oh pizza you're my favorite fooooood"]
   ]
 }
 
 const HomePage = () => {
-
   return (
     <>
-    <section id="first-page" className="grid">
+      <section id='first-page' className='grid'>
         <div></div>
-        <div className="intro">
-          <h1 id="intro-title">Instant Improv</h1>
-          <h2 id="intro-subtitle">Connect and chat away</h2>
-          <Link to="/chatroom/">
-            <button className="enter-chatroom-button btn">Enter Chatroom</button>
+        <div className='intro'>
+          <h1 id='intro-title'>Instant Improv</h1>
+          <h2 id='intro-subtitle'>Connect and chat away</h2>
+          <div>
+            <img className='visit-chatroom-image' src={heroBoy} alt='Picture of costumed boy hero' />
+          </div>
+          <Link to='/chatroom/'>
+            <button className='visit-chatroom-button btn'>Visit Chatroom</button>
           </Link>
-          <p id="homepage-tip">Most popular time is <span className="homepage-tip-best-time">Sunday 8:00pm EST</span></p>
+          <p id='homepage-tip'>
+            Most popular time is <span className='homepage-tip-best-time'>Sunday 8:00pm EST</span>
+          </p>
         </div>
         <div></div>
-      <Chatbox chat={firstChat}/>
-      <div></div>
-    </section>
+        <Chatbox chat={firstChat} />
+        <div></div>
+      </section>
 
-    <EmailSignup />
+      <EmailSignup />
 
-    <section id="second-page" className="grid">
-      <div></div>
-      <div className="list-items-container">
-        <ListItems />
+      <section id='second-page' className='grid'>
+        <div></div>
+        <div className='list-items-container'>
+          <ListItems />
+        </div>
+        <div></div>
+        <Chatbox chat={secondChat} />
+        <div></div>
+      </section>
+      <div id='home-footer'>
+        <h2 className='call-to-action'>Who will you be?</h2>
+        <Link to='/chatroom/'>
+          <button className='visit-chatroom-button btn'>Visit Chatroom</button>
+        </Link>
       </div>
-      <div></div>
-      <Chatbox chat={secondChat}/>
-      <div></div>
-    </section>
-    <div id="home-footer">
-      <h2 className="call-to-action">Who will you be?</h2>
-      <Link to="/chatroom/">
-        <button className="enter-chatroom-button btn">Enter Chatroom</button>
-      </Link>
-    </div>
     </>
   )
 }
