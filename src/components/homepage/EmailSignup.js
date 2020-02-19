55e56c03ab95af1640bceacafbd6634bb8acf300
Moshe Siegel <siegel.moshes@gmail.com>
2020-02-18 19:48:49 -0500

    Updated email sign up text

--- src/components/homepage/EmailSignup.js
+++ src/components/homepage/EmailSignup.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const EmailSignup = () => {
   const signupForm = {
@@ -15,7 +15,7 @@ const EmailSignup = () => {
     marginRight: 'auto',
     borderRadius: '5px'
   }
-  
+
   const largeFont = {
     fontSize: '21px'
   }
@@ -35,27 +35,54 @@ const EmailSignup = () => {
 
   const submitButton = {
     ...largeFont,
-    background: '#87002A',
+    background: '#87002A'
   }
 
   return (
     <>
-      <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css"/>
-      <div id="mc_embed_signup" style={signupForm}>
-      <form action="https://4scorechat.us19.list-manage.com/subscribe/post?u=862b467d49bd233e9d3f4bab2&amp;id=54efc95efc" 
-       method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate
-       style={formNoBottomMargin}>
-          <div id="mc_embed_signup_scroll">
-        <label htmlFor="mce-EMAIL" style={largeFont}>Sign up for our 4ScoreChat newsletter</label>
-        <input type="email" defaultValue="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required style={mediumFont}/>
-          {/*<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->*/}
-          <div style={fakeInput}
-           aria-hidden="true"><input type="text" name="b_862b467d49bd233e9d3f4bab2_54efc95efc" tabIndex="-1" defaultValue=""/></div>
-          <div className="clear"><input type="submit" style={submitButton} defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
+      <link href='//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css' rel='stylesheet' type='text/css' />
+      <div id='mc_embed_signup' style={signupForm}>
+        <form
+          action='https://4scorechat.us19.list-manage.com/subscribe/post?u=862b467d49bd233e9d3f4bab2&amp;id=54efc95efc'
+          method='post'
+          id='mc-embedded-subscribe-form'
+          name='mc-embedded-subscribe-form'
+          className='validate'
+          target='_blank'
+          noValidate
+          style={formNoBottomMargin}
+        >
+          <div id='mc_embed_signup_scroll'>
+            <label htmlFor='mce-EMAIL' style={largeFont}>
+              Sign up to get notified when we release new features
+            </label>
+            <input
+              type='email'
+              defaultValue=''
+              name='EMAIL'
+              className='email'
+              id='mce-EMAIL'
+              placeholder='email address'
+              required
+              style={mediumFont}
+            />
+            {/*<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->*/}
+            <div style={fakeInput} aria-hidden='true'>
+              <input type='text' name='b_862b467d49bd233e9d3f4bab2_54efc95efc' tabIndex='-1' defaultValue='' />
+            </div>
+            <div className='clear'>
+              <input
+                type='submit'
+                style={submitButton}
+                defaultValue='Subscribe'
+                name='subscribe'
+                id='mc-embedded-subscribe'
+                className='button'
+              />
+            </div>
           </div>
-      </form>
+        </form>
       </div>
-
     </>
   )
 }
