76196dab3799e1485ad43b54d713a890f9a5c524
Moshe Siegel <siegel.moshes@gmail.com>
2020-08-09 21:19:21 -0400

    Added timer to waiting for peer message

--- src/components/chatroomPage/WaitingForPeerMessage.js
+++ src/components/chatroomPage/WaitingForPeerMessage.js
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './WaitingForPeerMessage.css'
 
 const WaitingForPeerMessage = ({ cancelSearch }) => {
+  const [timer, setTimer] = useState(0)
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer(timer => timer + 1)
+    }, 1000)
+    return () => clearInterval(timerInterval)
+  }, [])
+
+  const waitingMessages = [
+    'Looking for someone 🧐',
+    'Casting a searching spell 🧙🏽',
+    'Requesting a funny fellow 🥳',
+    'Examining for clues 🔍',
+    'Sniffing for an improv pal 🐶',
+    'Calling in backup searchers 🗣',
+    'Whoops! My computer brain went 💩',
+    'Ahhhhhhhhh, noooooooooo 🙀',
+  ]
+
+  const timeToShowNoUsers = 30
+
+  function getWaitingMessage() {
+    const intervalBetweenMessages = timeToShowNoUsers / (waitingMessages.length - 1)
+    const potentialWaitingI = Math.floor(timer / intervalBetweenMessages)
+    const waitingI = Math.min(potentialWaitingI, waitingMessages.length - 1)
+    return waitingMessages[waitingI]
+  }
+
+  function renderTimer(timer) {
+    if (timer >= 3600) return '59:59'
+
+    let minutes = Math.floor(timer / 60).toString()
+    let seconds = Math.floor(timer % 60).toString()
+
+    if (minutes.length === 1) minutes = '0' + minutes
+    if (seconds.length === 1) seconds = '0' + seconds
+
+    return `${minutes} : ${seconds}`
+  }
+
   return (
     <div className='waiting-for-peer'>
-      <p className='waiting-title'>Looking for someone...</p>
+      <p className='waiting-title'>{getWaitingMessage()}</p>
+      <p className='timer'>{renderTimer(timer)}</p>
       <button className='btn' id='cancel-chat-btn' onClick={cancelSearch}>
         Cancel
       </button>
-      <p className='waiting-note'>
-        Best chatting time is <span className='waiting-best-time'>Sunday 8:00pm EST</span>. Come then to get matched
-        instantly!
-      </p>
+      {timer >= timeToShowNoUsers && (
+        <p className='waiting-note'>
+          Looks like no other users are online now. Best chatting time is{' '}
+          <span className='waiting-best-time'>Sunday 8:00pm EST</span>. Come then to get matched instantly!
+        </p>
+      )}
     </div>
   )
 }
